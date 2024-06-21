96bf1f23b1007c433e0fa6cefae0cff80f76582d
BLANK <JanTV720@gmail.com>
2024-06-21 08:57:07 +0200

    initial commit

---
+++ JSON.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudFlare_DynDNS
+{
+    public class JSON
+    {
+        public class ListDNSRecords
+        {
+            // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+            public class Data
+            {
+                public string content { get; set; }
+                public int weight { get; set; }
+            }
+
+            public class Meta
+            {
+                public bool auto_added { get; set; }
+                public bool managed_by_apps { get; set; }
+                public bool managed_by_argo_tunnel { get; set; }
+                public string source { get; set; }
+            }
+
+            public class Result
+            {
+                public string id { get; set; }
+                public string zone_id { get; set; }
+                public string zone_name { get; set; }
+                public string name { get; set; }
+                public string type { get; set; }
+                public string content { get; set; }
+                public bool proxiable { get; set; }
+                public bool proxied { get; set; }
+                public int ttl { get; set; }
+                public bool locked { get; set; }
+                public Meta meta { get; set; }
+                public DateTime created_on { get; set; }
+                public DateTime modified_on { get; set; }
+                public int? priority { get; set; }
+                public Data data { get; set; }
+            }
+
+            public class ResultInfo
+            {
+                public int page { get; set; }
+                public int per_page { get; set; }
+                public int count { get; set; }
+                public int total_count { get; set; }
+                public int total_pages { get; set; }
+            }
+
+                public List<Result> result { get; set; }
+                public bool success { get; set; }
+                public List<object> errors { get; set; }
+                public List<object> messages { get; set; }
+                public ResultInfo result_info { get; set; }
+        }
+        public class DNSRequest
+        {
+            public string type { get; set; }
+            public string name { get; set; }
+            public string content { get; set; }
+            public int ttl { get; set; }
+            public bool proxied;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Net;
+
+public class Program
+{
+    static string CachedIP = "1";
+    private static string[] Blacklist => File.ReadAllLines(Directory.GetCurrentDirectory() + "/Blacklist.txt");
+    private static string Token => File.ReadAllText(Directory.GetCurrentDirectory() + "/Token.txt");
+    public static void Main()
+    {
+        string? Token = Environment.GetEnvironmentVariable("Token");
+        string[]? Zones = Environment.GetEnvironmentVariable("Zones")?.Split(';');
+        string[]? StaticIP = Environment.GetEnvironmentVariable("StaticIP")?.Split(';');
+        if (Token == null)
+        {
+            Console.WriteLine($"No Account Token Provided");
+        }
+        if (Zones == null)
+        {
+            Console.WriteLine("No ZoneIDs Provided");
+        }
+        while (true)
+        {
+            string ip = GetPublicIP();
+            if (ip == "0")
+            {
+                Console.WriteLine("Couldnt Fetch Public IP!");
+                Thread.Sleep(10000);
+                continue;
+            }
+            if (ip != CachedIP)
+            {
+                CachedIP = ip;
+                foreach (var Zone in Zones)
+                {
+                    try
+                    {
+                        var DNSRecords = ListDNSRecords(Zone);
+                        foreach (var Record in DNSRecords.result)
+                        {
+                            try
+                            {
+                                if (Blacklist.Contains(Record.content)) continue;
+                                if (Record.content == ip) continue;
+                                if (Record.type != "A") continue;
+                                var b = new CloudFlare_DynDNS.JSON.DNSRequest() { content = ip, name = Record.name, ttl = Record.ttl, type = "A", proxied = Record.proxied };
+                                UpdateDNS(b, Record.id, Zone);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                            }
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    static string GetPublicIP()
+    {
+        string ip = "";
+        try
+        {
+            ip = new WebClient().DownloadString("https://api.ipify.org");
+            return ip;
+        }
+        catch
+        {
+        }
+        try
+        {
+            ip = new WebClient().DownloadString("https://ip.seeip.org");
+        }
+        catch
+        {
+            return "0";
+        }
+        return ip;
+    }
+    public static CloudFlare_DynDNS.JSON.ListDNSRecords ListDNSRecords(string zoneid)
+    {
+        Console.WriteLine("DNS List Request");
+        var url = $"https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records";
+
+        var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+        httpRequest.Headers["Authorization"] = "Bearer "+Token;
+
+
+        var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<CloudFlare_DynDNS.JSON.ListDNSRecords>(streamReader.ReadToEnd());
+        }
+    }
+    public static void UpdateDNS(CloudFlare_DynDNS.JSON.DNSRequest dns, string id, string zoneid)
+    {
+        var url = $"https://api.cloudflare.com/client/v4/zones/{zoneid}/dns_records/"+id;
+        Console.WriteLine(url);
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(dns, Newtonsoft.Json.Formatting.Indented);
+        Console.WriteLine(json);
+        var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+        httpRequest.Method = "PUT";
+        httpRequest.Headers["Authorization"] = "Bearer "+Token;
+        httpRequest.ContentType = "application/json";
+        var data = json;
+        using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+        {
+            streamWriter.Write(data);
+        }
+        var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            var result = streamReader.ReadToEnd();
+        }
+        Console.WriteLine(httpResponse.StatusCode);
+        Console.WriteLine($"Updated {dns.name} to {dns.content}");
+    }
+}
